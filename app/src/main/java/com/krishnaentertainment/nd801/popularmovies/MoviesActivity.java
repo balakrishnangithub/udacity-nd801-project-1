231f239e70904e19040049fd5497016b7a8171cc
Balakrishnan <light.bala@gmail.com>
2016-04-01 22:43:28 +0530

    Rename MainActivity to MoviesActivity

--- app/src/main/java/com/krishnaentertainment/nd801/popularmovies/MoviesActivity.java
+++ app/src/main/java/com/krishnaentertainment/nd801/popularmovies/MoviesActivity.java
@@ -3,7 +3,7 @@ package com.krishnaentertainment.nd801.popularmovies;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MoviesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

7085868f767a35ec6362b3ccbb3989076fbb81e5
Balakrishnan <light.bala@gmail.com>
2016-04-03 16:52:09 +0530

    Add GlobalConstants

---
+++ app/src/main/java/com/krishnaentertainment/nd801/popularmovies/models/GlobalConstants.java
@@ -0,0 +1,27 @@
+package com.krishnaentertainment.nd801.popularmovies.models;
+
+import com.krishnaentertainment.nd801.popularmovies.BuildConfig;
+
+public class GlobalConstants {
+    public static final String TMDB_API_KEY = BuildConfig.TMDB_API_KEY;
+    public static final String TMDB_MOVIE_BASE_URL = "http://api.themoviedb.org/3/discover/movie";
+    public static final String TMDB_IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+    public static final String TMDB_SORT_DESC = ".desc";
+    public static final String TMDB_SORT_ASC = ".asc";
+
+    public final static String TMDB_LARGE_POSTER = "w342"; // gallery view
+    public final static String TMDB_SMALL_POSTER = "w185"; // detail view
+
+    public static final String TMDB_PARAM_PAGE = "page";
+    public static final String TMDB_PARAM_API_KEY = "api_key";
+
+    public static final String TMDB_PARAM_ID = "id";
+    public static final String TMDB_PARAM_TITLE = "title";
+    public static final String TMDB_PARAM_POSTER_PATH = "poster_path";
+    public static final String TMDB_PARAM_BACKDROP_PATH = "backdrop_path";
+    public static final String TMDB_PARAM_OVERVIEW = "overview";
+    public static final String TMDB_PARAM_RELEASE_DATE = "release_date";
+    public static final String TMDB_PARAM_POPULARITY = "popularity";
+    public static final String TMDB_PARAM_VOTE_AVERAGE = "vote_average";
+    public static final String TMDB_PARAM_VOTE_COUNT = "vote_count";
+}
